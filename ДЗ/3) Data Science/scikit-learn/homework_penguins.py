6937e4ac6687925c224c3ffe3b506aac0a8bb730
Roma <roma.mevednikov@gmail.com>
2025-07-14 10:49:49 +0300

    commit

--- ДЗ/3) Data Science/scikit-learn/homework_penguins.py
+++ ДЗ/3) Data Science/scikit-learn/homework_penguins.py
@@ -19,35 +19,59 @@ print(penguins)
 # Розподіл даних на тренуючи та тестові в співвідношенні 80/20 (відповідно)
 X = penguins.drop(columns=['species'])
 y = penguins['species']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42 )
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 # Створення конвеєру для передобробки
-model = RandomForestClassifier()
+model = RandomForestClassifier(class_weight='balanced', random_state=42, max_depth=5)
 pipeline = Pipeline([
     ('scaler', StandardScaler()), # Масштабування
     ('classifier', model), # Класифікація
 ])
 # Звертаємося до моделі у конвєері та передаємо гіперпараметри
-param_grid = {'classifier__n_estimators': [50, 100, 150], 'classifier__max_depth': [5,10,15]}
+param_grid = {'classifier__n_estimators': [50, 100, 150],
+              'classifier__max_depth': [5,10,15],
+              'classifier__min_samples_split': [2, 5, 10],
+              'classifier__min_samples_leaf': [1, 2, 5],
+              'classifier__max_features': ['sqrt', 'log2', None],
+
+              }
 grid_search = GridSearchCV(pipeline, param_grid, cv=5) # Створюємо модель пошуку
 grid_search.fit(X_train, y_train)
+pipeline.fit(X_train, y_train)
 print("Найкращі параметри:", grid_search.best_params_)
 
 best_model = grid_search.best_estimator_ # Робимо удосконалену модель
 
 # Тренуємо та робимо предікт
-y_pred = best_model.predict(X_test)
+y_pred_1 = best_model.predict(X_test)
+y_pred_2 = pipeline.predict(X_test)
 
 # Оцінка точності, recall, precision F1-score
-scores = cross_val_score(best_model, X_train, y_train, cv=5)
-print(f'Результати: {scores.mean() * 100:.2f}%') # Результати крос - валідації для тренуючих даних
+scores_1 = cross_val_score(best_model, X_train, y_train, cv=5)
+print(f'Результати: {scores_1.mean() * 100:.2f}%') # Результати крос - валідації для тренуючих даних
 print(f'Точність: {best_model.score(X_test, y_test) * 100:.2f}%') # Точність моделі на тестових даних
-print(f'Recall Score: {recall_score(y_test, y_pred, average='weighted')* 100:.2f}%') # Recall Score
-print(f'Precision Score: {precision_score(y_test, y_pred, average='weighted')* 100:.2f}%') # Precision Score
-print(f'F1 - score: {f1_score(y_test, y_pred, average='weighted')* 100:.2f}%') # F1 - Score
+print(f'Recall Score: {recall_score(y_test, y_pred_1, average='weighted')* 100:.2f}%') # Recall Score
+print(f'Precision Score: {precision_score(y_test, y_pred_1, average='weighted')* 100:.2f}%') # Precision Score
+print(f'F1 - score: {f1_score(y_test, y_pred_1, average='weighted')* 100:.2f}%') # F1 - Score
+# Порівнняня з початковою моделлю
+print()
+scores_2 = cross_val_score(pipeline, X_train, y_train, cv=5)
+print(f'Результати: {scores_2.mean() * 100:.2f}%') # Результати крос - валідації для тренуючих даних
+print(f'Точність: {pipeline.score(X_test, y_test) * 100:.2f}%') # Точність моделі на тестових даних
 
 # Візуально відображаємо матрицю помилок
-disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, y_pred), display_labels=best_model.classes_)
-disp.plot(cmap='Blues')
+disp1 = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, y_pred_1), display_labels=best_model.classes_)
+disp1.plot(cmap='Blues')
 plt.title('Confusion Matrix')
 plt.show()
+# Для початкової моделі також
+disp2 = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, y_pred_2), display_labels=pipeline.classes_)
+disp2.plot(cmap='Greens')
+plt.title('Confusion Matrix')
+plt.show()
+
+# ВИСНОВКИ
+# Початкова модель показує результати на пів відсотка менше (99.25%), ніж краща модель (99.62%). З інших тестів у них по 100%.
+# Можна спробувати покращити результати крос - валідації до 100%. Матриця помилок також вказує, що вони все розподілили правильно.
+# Типові помилки можуть бути ігнорування пустих значеннь, переобучення, та незбалансованість класів. Щодо планів на подальше
+# дослідження, то можна спробувати використати інші класифікатори і обробити незбалансовані дані.
