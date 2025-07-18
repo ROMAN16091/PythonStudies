0d0ce711bdad6d09f87faaa15c4b183dda7d32d6
Roma <roma.mevednikov@gmail.com>
2025-07-18 15:52:45 +0300

    commit

--- ДЗ/3) Data Science/scikit-learn/step_project_ds.py
+++ ДЗ/3) Data Science/scikit-learn/step_project_ds.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 # Завантажуємо набір даних
 url = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
 df = pd.read_csv(url)
-print(df.head())
+print(df.head(),'\n')
 
 # Обробляємо відсутні значення
 print(df.isna().sum(),'\n')
@@ -13,18 +14,19 @@ df['Age'] = df['Age'].fillna(df['Age'].mean()) # Заповняємо відсу
 df['Deck'] = df['Cabin'].str[0] # Створюємо колонку Deck, яка вказує на палубу пасажира
 df['Deck'] = df['Deck'].fillna(df['Deck'].mode()[0]) # Заповняємо відсутню палубу модою
 df = df.drop(columns='Cabin') # Видаляємо колонку з каютою
-print(df.isna().sum())
+print(df.isna().sum(),'\n')
 
 # Кодуємо категоріальні змінні
 df = pd.get_dummies(df, columns=['Embarked']) # Кодуємо порт з якого пасажир сів на корабель
 df = pd.get_dummies(df, columns=['Sex']) # Кодуємо стать людей
-print(df.head())
+print(df.head(),'\n')
 
 # Видаляємо характеристики, які можуть не вносити вкладу в модель
 df = df.drop(columns='Ticket')
 
 # Аналіз розподілу ключових характеристик та їх зв'язку з цільовою змінною Survived
 
+num_df = df.select_dtypes(include='number')
 
 
 
@@ -32,14 +34,16 @@ df = df.drop(columns='Ticket')
 
 # Інженерія ознак
 df['FamilySize'] = df['SibSp'] + df['Parch'] + 1 # Розмір сім‘ї
-print(df['FamilySize'], '\n')
+print(df['FamilySize'],'\n')
 
 df['IsAlone'] = ((df['SibSp'] == 0) & (df['Parch'] == 0)) # Чи був пасажир один на кораблі
 print(df['IsAlone'],'\n')
 
 df['Title'] = df['Name'].str.split(', ').str[1].str.split('. ').str[0] # Робимо колонку з титулом
-print(df['Title'])
+print(df['Title'],'\n')
+df = df.drop(columns='Name') # Видаляємо колонку з іменами
 
+# Масштабування ознак
 
 
 
